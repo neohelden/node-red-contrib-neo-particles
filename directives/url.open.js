510082257e23569abf1fdb4a834ddb3140f4d4f8
Maik Hummel <yo@beevelop.com>
2018-10-16 10:05:19 +0200

    Directive: Add url.open

---
+++ directives/url.open.js
@@ -0,0 +1,34 @@
+module.exports = function (RED) {
+  function NeoDirectiveURLOpen(n) {
+    RED.nodes.createNode(this, n);
+    var node = this;
+
+    node.on('input', function (msg) {
+      if (typeof msg.payload !== 'object') {
+        node.error('Payload is not an object', msg)
+        return
+      }
+
+      if (!('response' in msg.payload)) {
+        msg.payload.response = {
+          directives: []
+        }
+      }
+
+      if (!('directives' in msg.payload.response)) {
+        msg.payload.response.directives = []
+      }
+
+      msg.payload.response.directives.push({
+        type: 'url.open',
+        data: {
+          url: n.url
+        }
+      })
+
+      node.send(msg)
+    });
+  }
+
+  RED.nodes.registerType("directive-url-open", NeoDirectiveURLOpen);
+}
